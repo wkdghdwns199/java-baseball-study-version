39bcc1939464a5bd238493060cf36c99af617609
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-11-27 13:07:24 +0900

    Feat : 안녕하세요ㅕ

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+<<<<<<< HEAD
+        System.out.println("안녕! 나는 클레오파트라");
+=======
+        System.out.println("안녕하세요!");
+>>>>>>> origin/feature/99
+    }
+}
